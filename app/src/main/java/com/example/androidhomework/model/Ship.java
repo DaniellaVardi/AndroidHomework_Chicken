b682402b72501602331024b9ee2ca385159d5786
Asaf Vardi <asafvardi@h-MacBook-Air-sl-Asaf.local>
2024-06-18 19:34:14 +0300

    final project

--- app/src/main/java/com/example/androidhomework/model/Ship.java
+++ app/src/main/java/com/example/androidhomework/model/Ship.java
@@ -5,14 +5,27 @@ import com.example.androidhomework.R;
 
 public class Ship extends Object {
 
+    private int life;
+
     public Ship(int x, int y) {
         super(x, y);
         image = R.drawable.rocket;
     }
 
+    public int getLife() {
+        return life;
+    }
+
+    public Ship setLife(int life) {
+        this.life = life;
+        return this;
+    }
+
     public void move(int dir){
         x += dir;
-        if(x<0) x=0;
-        if(x> Const.LENGTH-1) x = Const.LENGTH-1;
+        if (x < 0) x = 0;
+        if (x > Const.LENGTH-1) x = Const.LENGTH-1; // Restrict movement to array of size 3
     }
+
+
 }
